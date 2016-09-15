bb1c8deeb4a8009efa1056beef0f1c34c8a183f2
Brian Caruso <carusobp@gmail.com>
2016-09-14 17:31:09 -0700

    Add ConstructorInfoExtensions.CreateFunc[0-9] for consistency with MethodInfoExtensions

--- src/Invio.Extensions.Reflection/ConstructorInfoExtensions.cs
+++ src/Invio.Extensions.Reflection/ConstructorInfoExtensions.cs
@@ -89,6 +89,135 @@ namespace Invio.Extensions.Reflection {
             return Expression.Lambda<Func<object[], object>>(body, array).Compile();
         }
 
+        public static Func<object> CreateFunc0(this ConstructorInfo constructor) {
+            CheckParameters(constructor, expected: 0);
+
+            var body = Expression.Convert(
+                Expression.New(constructor),
+                typeof(object)
+            );
+
+            return Expression.Lambda<Func<object>>(body).Compile();
+        }
+
+        public static Func<object, object>
+            CreateFunc1(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 1);
+
+            return CreateFunc<Func<object, object>>(constructor);
+        }
+
+        public static Func<object, object, object>
+            CreateFunc2(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 2);
+
+            return CreateFunc<Func<object, object, object>>(constructor);
+        }
+
+        public static Func<object, object, object, object>
+            CreateFunc3(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 3);
+
+            return CreateFunc<Func<object, object, object, object>>(constructor);
+        }
+
+        public static Func<object, object, object, object, object>
+            CreateFunc4(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 4);
+
+            return CreateFunc<Func<object, object, object, object, object>>(constructor);
+        }
+
+        public static Func<object, object, object, object, object, object>
+            CreateFunc5(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 5);
+
+            return CreateFunc<Func<object, object, object, object, object, object>>(constructor);
+        }
+
+        public static Func<object, object, object, object, object, object, object>
+            CreateFunc6(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 6);
+
+            return CreateFunc<Func<object, object, object, object, object, object, object>>(constructor);
+        }
+
+        public static Func<object, object, object, object, object, object, object, object>
+            CreateFunc7(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 7);
+
+            return CreateFunc<Func<object, object, object, object, object, object, object, object>>(
+                constructor
+            );
+        }
+
+        public static Func<object, object, object, object, object, object, object, object, object>
+            CreateFunc8(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 8);
+
+            return CreateFunc<Func<object, object, object, object, object, object, object, object, object>>(
+                constructor
+            );
+        }
+
+        public static Func<object, object, object, object, object, object, object, object, object, object>
+            CreateFunc9(this ConstructorInfo constructor) {
+
+            CheckParameters(constructor, expected: 9);
+
+            return CreateFunc<Func<object, object, object, object, object, object, object, object, object, object>>(
+                constructor
+            );
+        }
+
+        private static void CheckParameters(ConstructorInfo constructor, int expected) {
+            if (constructor == null) {
+                throw new ArgumentNullException(nameof(constructor));
+            }
+
+            var parameters = constructor.GetParameters();
+
+            if (parameters.Length != expected) {
+                throw new ArgumentException(
+                    $"The '{nameof(constructor)}' argument must reference a " +
+                    $"{typeof(ConstructorInfo).Name} with {expected:N0} parameters.",
+                    nameof(constructor)
+                );
+            }
+        }
+
+        private static TFunc CreateFunc<TFunc>(ConstructorInfo constructor) {
+            var parameters = constructor.GetParameters();
+
+            Func<ParameterInfo, int, ParameterExpression> toArgument =
+                (info, index) => Expression.Parameter(typeof(object), $"argument{index}");
+
+            var arguments = parameters.Select(toArgument).ToArray();
+
+            Func<ParameterInfo, ParameterExpression, Expression> convert =
+                (info, expression) => Expression.Convert(expression, info.ParameterType);
+
+            var body = Expression.Convert(
+                Expression.New(
+                    constructor,
+                    parameters
+                        .Zip(arguments, convert)
+                        .ToArray()
+                ),
+                typeof(object)
+            );
+
+            return Expression.Lambda<TFunc>(body, arguments).Compile();
+        }
+
     }
 
 }
