a680a2d721268b8326134c506ec6d9a899fb6a52
Brian Caruso <carusobp@gmail.com>
2016-11-08 22:28:51 -0800

    Clean up documentation and formatting for 'TypeExtensions'

--- src/Invio.Extensions.Reflection/TypeExtensions.cs
+++ src/Invio.Extensions.Reflection/TypeExtensions.cs
@@ -1,31 +1,41 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Invio.Extensions.Reflection {
+
+    /// <summary>
+    ///   Extension methods to add functionality to instances of <see cref="Type" />.
+    /// </summary>
     public static class TypeExtensions {
 
         /// <summary>
-        /// Makes a string that contains the type name with it's generic parameters.
+        ///   Makes a <see cref="String" /> that contains the type name with its
+        ///   closing generic type arguments.
         /// </summary>
         /// <param name="type">
         ///   The <see cref="Type" /> instance the caller wants to create the named
         ///   string for.
         /// </param>
+        /// <returns>
+        ///   A <see cref="String" /> that is unique to the type specified for the
+        ///   fully qualified name with its closed generic type arguments.
+        /// </returns>
         /// <exception cref="ArgumentNullException">
         ///   Thrown when <paramref name="type" /> is null.
         /// </exception>
-        /// <returns>
-        ///   A string that is unique to the type specified for the fullyqualified name
-        ///   and generic types.
-        /// </returns>
         public static String GetNameWithGenericParameters(this Type type) {
             if (type == null) {
                 throw new ArgumentNullException(nameof(type));
             }
 
-            var genericTypes = String.Join(", ", type.GenericTypeArguments.Select(gt => gt.FullName));
-            var name = $"{type.Name}";
+            var genericTypes = String.Join(
+                ", ",
+                type.GenericTypeArguments.Select(genericType => genericType.FullName)
+            );
+
+            var name = type.Name;
 
             if (!String.IsNullOrWhiteSpace(genericTypes)) {
                 name += $"<{genericTypes}>";
@@ -34,4 +44,5 @@ namespace Invio.Extensions.Reflection {
             return name;
         }
     }
+
 }
