911858f16815549e47a823821db64c7fb1940585
Brian Caruso <carusobp@gmail.com>
2017-07-19 15:01:29 -0700

    Update build scripts to use dotnet v1.0.4

--- build.sh
+++ build.sh
@@ -9,21 +9,9 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-dotnet restore
+dotnet restore ./src/Invio.Extensions.Reflection
+dotnet restore ./test/Invio.Extensions.Reflection.Tests
 
-# Ideally we would use the 'dotnet test' command to test netcoreapp and net46 so restrict for now
-# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+dotnet test ./test/Invio.Extensions.Reflection.Tests/Invio.Extensions.Reflection.Tests.csproj -c Release -f netcoreapp1.0
 
-dotnet test ./test/Invio.Extensions.Reflection.Tests -c Release -f netcoreapp1.0
-
-# Instead, run directly with mono for the full .net version
-dotnet build ./test/Invio.Extensions.Reflection.Tests -c Release -f net46
-
-mono \
-./test/Invio.Extensions.Reflection.Tests/bin/Release/net46/*/dotnet-test-xunit.exe \
-./test/Invio.Extensions.Reflection.Tests/bin/Release/net46/*/Invio.Extensions.Reflection.Tests.dll
-
-revision=${TRAVIS_JOB_ID:=1}
-revision=$(printf "%04d" $revision)
-
-dotnet pack ./src/Invio.Extensions.Reflection -c Release -o ./artifacts
+dotnet pack ./src/Invio.Extensions.Reflection -c Release -o ../../artifacts
--- set-debug-type.ps1
+++
@@ -1,4 +0,0 @@
-copy 'src\Invio.Extensions.Reflection\project.json' 'src\Invio.Extensions.Reflection\project.json.bak'
-$project = Get-Content 'src\Invio.Extensions.Reflection\project.json.bak' -raw | ConvertFrom-Json
-$project.buildOptions.debugType = "full"
-$project | ConvertTo-Json  | set-content 'src\Invio.Extensions.Reflection\project.json'
