5025217891ece0a5b6c37a7c72ddc1cfa71cce7e
Brian Caruso <carusobp@gmail.com>
2018-01-17 15:13:16 -0800

    Add 'IsDerivativeOf()' function to check inheritance while respecting open generics (#29)

--- src/Invio.Extensions.Reflection/TypeExtensions.cs
+++ src/Invio.Extensions.Reflection/TypeExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -10,6 +12,13 @@ namespace Invio.Extensions.Reflection {
     /// </summary>
     public static class TypeExtensions {
 
+        private static ConcurrentDictionary<Tuple<Type, Type>, bool>
+            isDerivativeOfCache { get; }
+
+        static TypeExtensions() {
+            isDerivativeOfCache = new ConcurrentDictionary<Tuple<Type, Type>, bool>();
+        }
+
         /// <summary>
         ///   Makes a <see cref="String" /> that contains the type name with its
         ///   closing generic type arguments.
@@ -18,13 +27,13 @@ namespace Invio.Extensions.Reflection {
         ///   The <see cref="Type" /> instance the caller wants to create the named
         ///   string for.
         /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="type" /> is null.
+        /// </exception>
         /// <returns>
         ///   A <see cref="String" /> that is unique to the type specified for the
         ///   fully qualified name with its closed generic type arguments.
         /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///   Thrown when <paramref name="type" /> is null.
-        /// </exception>
         public static String GetNameWithGenericParameters(this Type type) {
             if (type == null) {
                 throw new ArgumentNullException(nameof(type));
@@ -43,6 +52,110 @@ namespace Invio.Extensions.Reflection {
 
             return name;
         }
+
+        /// <summary>
+        ///   Determine whether or not the <see cref="Type" /> of interest,
+        ///   <paramref name="type" />, has the <paramref name="parentType" />
+        ///   somewhere in its hierarchy.
+        /// </summary>
+        /// <param name="type">
+        ///   The <see cref="Type" /> instance whose hierarchy will be checked to see
+        ///   if it is the same type as <paramref name="parentType" />, or if it
+        ///   a derivative of <paramref name="parentType" />.
+        /// </param>
+        /// <param name="parentType">
+        ///   The <see cref="Type" /> instance that may or may not be somewhere
+        ///   in the hierarchy of <paramref name="type" />.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="type" /> or <paramref name="parentType" /> is null.
+        /// </exception>
+        /// <returns>
+        ///   This method returns <c>true</c> if one of the follow situations is met:
+        ///   <list type="bullet">
+        ///     <item>
+        ///       <description>
+        ///         The <paremref name="type" /> and <paramref name="parentType" /> are
+        ///         the same <see cref="Type" />.
+        ///       </description>
+        ///     </item>
+        ///     <item>
+        ///       <description>
+        ///         The <paramref name="parentType" /> is either not a generic
+        ///         <see cref="Type" />, or a closed generic <see cref="Type" />
+        ///         and <paramref name="type" /> is assignable to that
+        ///         <see cref="Type" />.
+        ///       </description>
+        ///     </item>
+        ///     <item>
+        ///       <description>
+        ///         The <paramref name="parentType" /> is an open generic
+        ///         <see cref="Type" />, and <paramref name="type" /> either
+        ///         implements that <see cref="Type" />, or inherits from a
+        ///         <see cref="Type" /> that implements that <see cref="Type" />.
+        ///       </description>
+        ///     </item>
+        ///     <item>
+        ///       <description>
+        ///         The <paremref name="type" /> and <paramref name="parentType" /> are
+        ///         the same <see cref="Type" />.
+        ///       </description>
+        ///     </item>
+        ///   </list>
+        ///   In all other situations, this method returns <c>false</c>.
+        /// </returns>
+        public static bool IsDerivativeOf(this Type type, Type parentType) {
+            if (type == null) {
+                throw new ArgumentNullException(nameof(type));
+            } else if (parentType == null) {
+                throw new ArgumentNullException(nameof(parentType));
+            }
+
+            return isDerivativeOfCache.GetOrAdd(
+                Tuple.Create(type, parentType),
+                tuple => IsDerivativeOfImpl(tuple.Item1, tuple.Item2)
+            );
+        }
+
+        private static bool IsDerivativeOfImpl(Type type, Type parentType) {
+            if (type == parentType || parentType == typeof(object)) {
+                return true;
+            }
+
+            if (parentType.IsConstructedGenericType) {
+                return parentType.IsAssignableFrom(type);
+            }
+
+            var parentTypeInfo = parentType.GetTypeInfo();
+
+            if (!parentTypeInfo.IsGenericType) {
+                return parentType.IsAssignableFrom(type);
+            }
+
+            IEnumerable<Type> potentialMatches;
+
+            if (parentTypeInfo.IsInterface) {
+                potentialMatches = type.GetInterfaces();
+            } else {
+                potentialMatches = GetBaseTypes(type);
+            }
+
+            return
+                potentialMatches
+                    .Select(potentialMatch => potentialMatch.GetTypeInfo())
+                    .Where(potentialMatch => potentialMatch.IsGenericType)
+                    .Select(potentialMatch => potentialMatch.GetGenericTypeDefinition())
+                    .Any(potentialMatch => potentialMatch == parentType);
+        }
+
+        private static IEnumerable<Type> GetBaseTypes(Type type) {
+            while (type != null) {
+                yield return type;
+
+                type = type.GetTypeInfo().BaseType;
+            }
+        }
+
     }
 
 }
--- test/Invio.Extensions.Reflection.Tests/TypeExtensionsTests.cs
+++ test/Invio.Extensions.Reflection.Tests/TypeExtensionsTests.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 using Invio.Xunit;
 
 namespace Invio.Extensions.Reflection {
 
     [UnitTest]
-    public class TypeExtensionsTests {
+    public sealed class TypeExtensionsTests {
 
         [Fact]
         public void GetNameWithGenericParameters_ArgNull_Checks() {
@@ -43,7 +45,61 @@ namespace Invio.Extensions.Reflection {
             );
         }
 
+        [Fact]
+        public void IsDerivativeOf_NullExtendedType() {
+            Type type = null;
+
+            Assert.Throws<ArgumentNullException>(
+                () => type.IsDerivativeOf(typeof(Object))
+            );
+        }
+
+        [Fact]
+        public void IsDerivativeOf_NullParentType() {
+            var type = typeof(string);
+
+            Assert.Throws<ArgumentNullException>(
+                () => type.IsDerivativeOf(null)
+            );
+        }
+
+        [Theory]
+        [InlineData(typeof(object), typeof(object))]
+        [InlineData(typeof(ClassUnderTest<int, string>), typeof(ClassUnderTest<int, string>))]
+        [InlineData(typeof(ClassUnderTest<int, string>), typeof(ClassUnderTest<,>))]
+        [InlineData(typeof(List<string>), typeof(IList<string>))]
+        [InlineData(typeof(List<int>), typeof(IList<>))]
+        [InlineData(typeof(ArgumentNullException), typeof(ArgumentException))]
+        [InlineData(typeof(IList), typeof(IEnumerable))]
+        [InlineData(typeof(IList<>), typeof(IEnumerable<>))]
+        [InlineData(typeof(IList<>), typeof(IEnumerable))]
+        [InlineData(typeof(IEnumerable<string>), typeof(IEnumerable<object>))]
+        [InlineData(typeof(ChildUnderTest<int>), typeof(ClassUnderTest<int, string>))]
+        [InlineData(typeof(ChildUnderTest<>), typeof(ClassUnderTest<,>))]
+        [InlineData(typeof(GrandchildUnderTest), typeof(ClassUnderTest<int, string>))]
+        [InlineData(typeof(GrandchildUnderTest), typeof(ClassUnderTest<,>))]
+        [InlineData(typeof(GrandchildUnderTest), typeof(ChildUnderTest<int>))]
+        [InlineData(typeof(GrandchildUnderTest), typeof(ChildUnderTest<>))]
+        public void IsDerivativeOf_Matches(Type type, Type parentType) {
+            Assert.True(type.IsDerivativeOf(parentType));
+        }
+
+        [Theory]
+        [InlineData(typeof(object), typeof(string))]
+        [InlineData(typeof(ArgumentException), typeof(ArgumentNullException))]
+        [InlineData(typeof(IEnumerable<>), typeof(IEnumerable<string>))]
+        [InlineData(typeof(IDictionary<,>), typeof(IDictionary<int, string>))]
+        [InlineData(typeof(IEnumerable<object>), typeof(IEnumerable<string>))]
+        [InlineData(typeof(GrandchildUnderTest), typeof(ChildUnderTest<string>))]
+        public void IsDerivativeOf_Mismatches(Type type, Type parentType) {
+            Assert.False(type.IsDerivativeOf(parentType));
+        }
+
         private class ClassUnderTest<T, U> { }
+
+        private class ChildUnderTest<T> : ClassUnderTest<T, string> {}
+
+        private class GrandchildUnderTest : ChildUnderTest<int> {}
 
     }
 
